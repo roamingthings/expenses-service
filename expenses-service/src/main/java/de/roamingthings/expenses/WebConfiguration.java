8c863a8b02374e04077d99c9d513a6a2f5b7cc67
Alexander Sparkowsky <info@roamingthings.de>
2017-04-13 22:39:36 +0200

    Added home controller to index page and webjar resource

---
+++ expenses-service/src/main/java/de/roamingthings/expenses/WebConfiguration.java
@@ -0,0 +1,20 @@
+package de.roamingthings.expenses;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author Alexander Sparkowsky [info@roamingthings.de]
+ * @version 2017/04/13
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
