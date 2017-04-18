62ea2ba9768aedd49b083295cd66b35489a7c682
Alexander Sparkowsky <info@roamingthings.de>
2017-04-18 20:39:09 +0200

    Fixed web configuration

--- expenses-static-ui/src/main/java/de/roamingthings/expenses/ui/Config.java
+++ expenses-static-ui/src/main/java/de/roamingthings/expenses/ui/Config.java
@@ -2,14 +2,13 @@ package de.roamingthings.expenses.ui;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 
 /**
  * @author Alexander Sparkowsky [info@roamingthings.de]
  * @version 2017/04/17
  */
 @Configuration
-@EnableWebMvc
-@EnableHypermediaSupport(type= {EnableHypermediaSupport.HypermediaType.HAL})
+@EnableHypermediaSupport(type= {HypermediaType.HAL})
 public class Config {
 }
