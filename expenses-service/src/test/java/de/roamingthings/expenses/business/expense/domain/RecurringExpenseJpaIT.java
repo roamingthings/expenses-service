31126fa9afea2df97aca51eb05286775bb874a30
Alexander Sparkowsky <info@roamingthings.de>
2017-04-23 10:17:01 +0200

    Fixed date usind in IT

--- expenses-service/src/test/java/de/roamingthings/expenses/business/expense/domain/RecurringExpenseJpaIT.java
+++ expenses-service/src/test/java/de/roamingthings/expenses/business/expense/domain/RecurringExpenseJpaIT.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
-import java.util.Date;
+import java.time.LocalDate;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
@@ -53,7 +53,7 @@ public class RecurringExpenseJpaIT {
         return new RecurringExpense(
                     "Test description",
                     "label",
-                    new Date(),
+                    LocalDate.now(),
                     RecurrencePeriod.MONTHLY,
                     ExpenseType.SUBSCRIPTION,
                     BigDecimal.valueOf(1.23),
