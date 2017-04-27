f359e4bda0bcce351545ae9ea4697756eef9c79a
Alexander Sparkowsky <info@roamingthings.de>
2017-04-27 22:20:10 +0200

    Allowing entities to be deleted by AJAX

--- expenses-mono/src/main/java/de/roamingthings/expenses/ui/expense/controller/RecurringExpenseController.java
+++ expenses-mono/src/main/java/de/roamingthings/expenses/ui/expense/controller/RecurringExpenseController.java
@@ -2,11 +2,16 @@ package de.roamingthings.expenses.ui.expense.controller;
 
 import de.roamingthings.expenses.business.expense.domain.RecurringExpense;
 import de.roamingthings.expenses.business.expense.service.RecurringExpenseService;
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.time.LocalDate;
 
@@ -14,6 +19,7 @@ import java.time.LocalDate;
  * @author Alexander Sparkowsky [info@roamingthings.de]
  * @version 2017/04/15
  */
+@Log
 @Controller
 @RequestMapping("/recurring_expenses")
 public class RecurringExpenseController {
@@ -55,9 +61,16 @@ public class RecurringExpenseController {
         return "recurring_expenses/form";
     }
 
-    @RequestMapping(path = "/delete/{id}", method = RequestMethod.GET)
-    public String deleteRecurringExpense(@PathVariable Long id) {
+    @RequestMapping(path = "/delete/{id}", method = RequestMethod.DELETE)
+    public String deleteRecurringExpenseAjax(@PathVariable Long id, Model model, HttpServletRequest request) {
         recurringExpenseService.delete(id);
+
+        if ("DELETE".equals(request.getMethod())) {
+            final Iterable<RecurringExpense> recurringExpenseList = recurringExpenseService.findAllRecurringExpenseSummaries();
+            model.addAttribute("recurringExpenseList", recurringExpenseList);
+
+            return "recurring_expenses/summary_table";
+        }
 
         return "redirect:/recurring_expenses";
     }
