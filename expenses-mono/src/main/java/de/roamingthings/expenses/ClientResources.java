7bd7869a1a10e8f27be1e8463b7c7fadce67afd8
Alexander Sparkowsky <info@roamingthings.de>
2017-05-02 23:40:29 +0200

    Added GitHub and facebook authorization using OAuth2

---
+++ expenses-mono/src/main/java/de/roamingthings/expenses/ClientResources.java
@@ -0,0 +1,26 @@
+package de.roamingthings.expenses;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+
+/**
+ * @author Alexander Sparkowsky [info@roamingthings.de]
+ * @version 2017/05/01
+ */
+public class ClientResources {
+
+    @NestedConfigurationProperty
+    private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
+
+    @NestedConfigurationProperty
+    private ResourceServerProperties resource = new ResourceServerProperties();
+
+    public AuthorizationCodeResourceDetails getClient() {
+        return client;
+    }
+
+    public ResourceServerProperties getResource() {
+        return resource;
+    }
+}
