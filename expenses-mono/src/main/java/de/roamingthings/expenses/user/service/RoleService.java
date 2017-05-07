e0ceac2034d173dff10902060e06a42886894c7a
Alexander Sparkowsky <info@roamingthings.de>
2017-05-07 10:25:11 +0200

    Added service to add role if not exists

---
+++ expenses-mono/src/main/java/de/roamingthings/expenses/user/service/RoleService.java
@@ -0,0 +1,9 @@
+package de.roamingthings.expenses.user.service;
+
+/**
+ * @author Alexander Sparkowsky [info@roamingthings.de]
+ * @version 2017/05/07
+ */
+public interface RoleService {
+    void addRoleIfNotExists(String role);
+}
---
+++ expenses-mono/src/main/java/de/roamingthings/expenses/user/service/RoleServiceImpl.java
@@ -0,0 +1,25 @@
+package de.roamingthings.expenses.user.service;
+
+import de.roamingthings.expenses.user.domain.Role;
+import de.roamingthings.expenses.user.repository.RoleRepository;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Alexander Sparkowsky [info@roamingthings.de]
+ * @version 2017/05/07
+ */
+@Service
+public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public void addRoleIfNotExists(String role) {
+        if (!roleRepository.existsByRole(role)) {
+            Role roleToCreate = new Role(role);
+            roleRepository.save(roleToCreate);
+        }
+    }
+}
