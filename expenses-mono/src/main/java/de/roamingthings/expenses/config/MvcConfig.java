7b21b6223d6b9fab8a2c668dd7495456b0d84898
Alexander Sparkowsky <info@roamingthings.de>
2017-05-22 06:37:39 +0200

    [Improvement] Mapping frontend to webcontext root

--- expenses-mono/src/main/java/de/roamingthings/expenses/config/MvcConfig.java
+++ expenses-mono/src/main/java/de/roamingthings/expenses/config/MvcConfig.java
@@ -1,8 +1,17 @@
 package de.roamingthings.expenses.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * @author Alexander Sparkowsky [info@roamingthings.de]
@@ -14,6 +23,30 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        super.configurePathMatch(configurer);
+        configurer.setUseRegisteredSuffixPatternMatch(false);
+        configurer.setUseSuffixPatternMatch(false);
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/frontend/0.0.1/");
+    }
+
+    @Controller
+    static class IndexController {
+
+        @RequestMapping(value = "/", method = GET)
+        public void addRequestMapping(HttpServletResponse response) throws IOException {
+            response.sendRedirect("/index.html");
+        }
     }
 
 }
